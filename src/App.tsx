ad9920d91457d9b64f76dbea4057a0a9927312d8
Kevin Shah <kevin2252238@gmail.com>
2022-10-14 19:35:08 +0530

    created basic sign in page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
-import { Box, ThemeProvider, Typography } from "@mui/material";
-import { theme } from "./theme";
+import { Box, CssBaseline, ThemeProvider, ToggleButton } from "@mui/material";
+import { SignIn } from "./pages/SignIn";
+import { DarkMode, LightMode } from "@mui/icons-material";
+import { useState } from "react";
+import { lightTheme, darkTheme } from "./theme";
 
 function App() {
+  const [lightMode, setLightMode] = useState(true);
+  const icon = lightMode ? <LightMode /> : <DarkMode />;
+  const theme = (lightMode ? lightTheme : darkTheme);
+
   return (
     <ThemeProvider theme={theme}>
-      <div className="App">
-        <Box bgcolor={"background.default"}>
-          <Typography variant="h1">
-            Hello, World2!
-          </Typography>
-        </Box>
-      </div>
+      <CssBaseline />
+      <Box textAlign={"center"} sx={{ mt: 2 }}>
+        <ToggleButton value="Theme" onChange={() => setLightMode(!lightMode)}>
+          {icon}
+        </ToggleButton>
+        <br />
+        Toggle Theme
+      </Box>
+      <SignIn />
     </ThemeProvider>
   );
 }
