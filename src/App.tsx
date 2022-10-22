d67dbd7b2f4b3587176192d2fdc80829bb1ea243
Kevin Shah <kevin2252238@gmail.com>
2022-10-22 13:22:47 +0530

    Added React Router

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,31 @@ import { SignIn } from "./pages/SignIn";
 import { DarkMode, LightMode } from "@mui/icons-material";
 import { useState } from "react";
 import { lightTheme, darkTheme } from "./theme";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
 
 function App() {
-  const [darkMode, setDarkMode] = useState(true);
-  const theme = (darkMode ? darkTheme: lightTheme);
+  const [darkMode, setDarkMode] = useState(false);
+  const theme = (darkMode ? darkTheme : lightTheme);
 
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Box textAlign={"center"} alignItems={"center"} display="flex" justifyContent={"center"} sx={{ mt: 2 }}>
-        <LightMode fontSize={"small"} />
-        <Switch onChange={(e) => setDarkMode(e.target.checked)}></Switch>
-        <DarkMode fontSize={"small"} />
-      </Box>
-      <SignIn />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Box textAlign={"center"} alignItems={"center"} display="flex" justifyContent={"center"} sx={{ mt: 2 }}>
+          <LightMode fontSize={"small"} />
+          <Switch onChange={(e) => setDarkMode(e.target.checked)}></Switch>
+          <DarkMode fontSize={"small"} />
+        </Box>
+        <Routes>
+          <Route path="/" element={<SignIn />} />
+          <Route path="*" element={<Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+          ><h1>ERROR: Page Not Found</h1></Box>} />
+        </Routes>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
